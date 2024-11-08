69161101885d6da2fb33bee43cbc0964e05cefdf
Ilhan Polat <ilhanpolat@gmail.com>
2024-11-08 12:03:50 +0100

    SQ: sym real

--- scipy/sparse/linalg/_eigen/arpack/ARPACK/_arpack_s_single.h
+++ scipy/sparse/linalg/_eigen/arpack/ARPACK/_arpack_s_single.h
@@ -1,11 +1,19 @@
 #include "_arpack.h"
 
 // BLAS Routines used
-void saxpy_(int* n, double* alpha, double* x, int* incx, double* y, int* incy);
-void sswap_(int* n, double* x, int* incx, double* y, int* incy);
-void scopy_(int* n, double* x, int* incx, double* y, int* incy);
-void sscal_(int* n, double* alpha, double* x, int* incx);
-void sgemv_(char* trans, int* m, int* n, double* alpha, double* a, int* lda, double* x, int* incx, double* beta, double* y, int* incy);
+void saxpy_(int* n, float* alpha, float* x, int* incx, float* y, int* incy);
+void scopy_(int* n, float* x, int* incx, float* y, int* incy);
+void sgemv_(char* trans, int* m, int* n, float* alpha, float* a, int* lda, float* x, int* incx, float* beta, float* y, int* incy);
+void sger_(int* m, int* n, float* alpha, float* x, int* incx, float* y, int* incy, float* a, int* lda);
+float snrm2_(int* n, float* x, int* incx);
+void sscal_(int* n, float* alpha, float* x, int* incx);
+void sswap_(int* n, float* x, int* incx, float* y, int* incy);
+void strmm_(char* side, char* uplo, char* transa, char* diag, int* m, int* n, float* alpha, float* a, int* lda, float* b, int* ldb);
+
 
 // LAPACK Routines used
-void slartg_(double* f, double* g, double* c, double* s, double* r);
+void sgeqr2_(int* m, int* n, float* a, int* lda, float* tau, float* work, int* info);
+void slacpy_(char* uplo, int* m, int* n, float* a, int* lda, float* b, int* ldb);
+void slartg_(float* f, float* g, float* c, float* s, float* r);
+void sorm2r_(char* side, char* trans, int* m, int* n, int* k, float* a, int* lda, float* tau, float* c, int* ldc, float* work, int* info);
+void ssteqr_(char* compz, int* n, float* d, float* e, float* z, int* ldz, float* work, int* info);
